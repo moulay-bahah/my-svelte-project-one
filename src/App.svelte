9a52162dafbf395be3232e19abbcbefbc53b6f91
moulay <bahahmoulay@gmail.com>
2023-11-03 08:40:11 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<script>
+</script>
+
+<main>
+  <h2>التاريخ 2023/11/02</h2>
+  <table>
+    <tr>
+      <th>المدينة</th>
+      <th>محطة الوقود</th>
+      <th>مدة توفر الوقود</th>
+      <th>الموقع</th>
+    </tr>
+    <tr>
+      <td>سرت</td>
+      <td>محطة القدس</td>
+      <td>الثامنة صباحا حتى نفاد الوقود</td>
+      <td>مدينة سرت</td>
+    </tr>
+    <tr>
+      <td>سرت</td>
+      <td>محطة القدس</td>
+      <td>الثامنة صباحا حتى نفاد الوقود</td>
+      <td>مدينة سرت</td>
+    </tr>
+    <tr>
+      <td>سرت</td>
+      <td>محطة القدس</td>
+      <td>الثامنة صباحا حتى نفاد الوقود</td>
+      <td>مدينة سرت</td>
+    </tr>
+    <tr>
+      <td>سرت</td>
+      <td>محطة القدس</td>
+      <td>الثامنة صباحا حتى نفاد الوقود</td>
+      <td>مدينة سرت</td>
+    </tr>
+  </table>
+</main>
+
+<style>
+  table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }
+  
+  td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
+  
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
+  </style>
+
